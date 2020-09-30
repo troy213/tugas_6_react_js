5d62bdd566f1825200226ef3a589329052540ce9
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2020-09-30 15:02:04 +0700

    Tugas 6 React JS
    Memahami cara penggunaan CSS.
    Memahami cara installasi Routing.
    Memahami cara penerapan Routing.
    Memahami cara penggunaan Navigasi Routing.

---
+++ src/Lib/DaftarMakanan.js
@@ -0,0 +1,29 @@
+const DaftarMakanan = [
+  {
+    NamaMakanan: "Nasi Padang",
+    Harga: "20.000",
+    pic: "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg"
+  },
+  {
+    NamaMakanan: "Sate",
+    Harga: "20.000",
+    pic: "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+  },
+  {
+    NamaMakanan: "Soto",
+    Harga: "10.000",
+    pic: "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png"
+  },
+  {
+    NamaMakanan: "Uduk",
+    Harga: "10.000",
+    pic: "https://www.dbs.com/iwov-resources/images/newsroom/indonesia/Blog/masakan%20nusantara/nasi%20kentut.png"
+  },
+  {
+    NamaMakanan: "Nasi Kuning",
+    Harga: "15.000",
+    pic: "http://gourmetconviction.com/gc/wp-content/uploads/2020/01/NasiKuningKomplitLandscape-937x675.jpg"
+  }
+];
+
+export default DaftarMakanan;
